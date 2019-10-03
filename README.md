199f1e6d82ed895ef8097833313acb44fb0f6610
Chris Hubbard <chubbard@gmail.com>
2019-10-03 12:52:37 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Salesforce API Client
+
+This is a generic client that can be used to integrate with Salesforce APIs.
+
+### Requirements
+* Python (with pip)
+* requests
+
+### Getting Started
+
+It is recommended that you create a virtual environment and then simply install the requests library.
+
+#### Creating a virtual environment
+##### MacOS/Linux
+	python3 -m venv pyenv
+	source pyenv/bin/activate
+
+##### Windows
+	py -m venv pyenv
+	.\pyenv\Scripts\activate
+
+#### Install Requests
+
+	pip install requests
+
+### Usage
+SalesforceClient must be initialized with a login_endpoint, username, password, token, client_id, and client_secret. Once intialized, you can use any of the provided functions to return a requests response object.
+
+### Example
+```python
+from .client import SalesforceClient
+
+login_endpoint = "https://login.salesforce.com/services/oauth2/token"
+username = "my_username"
+password = "my_password"
+token = "my_security_token"
+client_id = "my_client_id"
+client_secret = "my_client_secret"
+
+client = SalesforceClient(login_endpoint, username, password, token, client_id, client_secret)
+response = client.login()
+if response.status_code == 200:
+	print("Login Success:", response.text)
+	query_response = client.query("select Id, Name from User")
+	print("Users:", query_response.text)
+else:
+	print("Salesforce login failed:", response.text)
+```
---
+++ client.py
@@ -0,0 +1,111 @@
+import requests, json
+
+class SalesforceClient:
+	# static variables
+	API_ENDPOINT = "/services/data/v46.0/"
+
+	# constructor
+	def __init__(self, login_endpoint, username, password, token, client_id, client_secret):
+		# instance variables
+		self.login_endpoint = login_endpoint
+		self.username = username
+		self.password = password
+		self.token = token
+		self.client_id = client_id
+		self.client_secret = client_secret
+
+	# login to Salesforce
+	def login(self):
+		payload = {
+			"grant_type": "password",
+			"client_id": self.client_id,
+			"client_secret": self.client_secret,
+			"username": self.username,
+			"password": self.password + self.token
+		} 
+		headers = { "Content-Type": "application/x-www-form-urlencoded" }
+		response = requests.post(self.login_endpoint, headers=headers, data=payload)
+		self.access_token = json.loads(response.text)["access_token"]
+		self.instance_url = json.loads(response.text)["instance_url"]
+		return response
+
+	def get_headers(self):
+		return {
+			"Content-Type": "application/json; charset=UTF-8",
+			"Accept": "application/json",
+			"Authorization": "Bearer "+ self.access_token
+		}
+
+	# query data using SOQL
+	def query(self, query):
+		url = self.instance_url + self.API_ENDPOINT +"query/?q="+ query
+		response = requests.get(url, headers=self.get_headers())
+		return response
+
+	# create record
+	def record_create(self, object_name, payload):
+		url = self.instance_url + self.API_ENDPOINT +"sobjects/"+ object_name +"/"
+		response = requests.post(url, headers=self.get_headers(), data=json.dumps(payload))
+		return response
+
+	# update record
+	def record_update(self, object_name, object_id, payload):
+		url = self.instance_url + self.API_ENDPOINT +"sobjects/"+ object_name +"/"+ object_id
+		response = requests.patch(url, headers=self.get_headers(), data=json.dumps(payload))
+		return response
+
+	# delete record
+	def record_delete(self, object_name, object_id):
+		url = self.instance_url + self.API_ENDPOINT +"sobjects/"+ object_name +"/"+ object_id
+		response = requests.delete(url, headers=self.get_headers())
+		return response
+
+	# get record with external id field
+	def record_get_extid(self, object_name, extid_field_name, ext_id):
+		url = self.instance_url + self.API_ENDPOINT +"sobjects/"+ object_name +"/"+ extid_field_name +"/"+ ext_id
+		response = requests.get(url, headers=self.get_headers())
+		return response
+
+	# upsert record with external id field
+	def record_upsert_extid(self, object_name, extid_field_name, ext_id, payload):
+		url = self.instance_url + self.API_ENDPOINT +"sobjects/"+ object_name +"/"+ extid_field_name +"/"+ ext_id
+		response = requests.patch(url, headers=self.get_headers(), data=json.dumps(payload))
+		return response
+
+	# delete record with external id field
+	def record_delete_extid(self, object_name, extid_field_name, ext_id):
+		url = self.instance_url + self.API_ENDPOINT +"sobjects/"+ object_name +"/"+ extid_field_name +"/"+ ext_id
+		response = requests.delete(url, headers=self.get_headers())
+		return response
+
+	# create job
+	def job_create(self, payload):
+		url = self.instance_url + self.API_ENDPOINT +"jobs/ingest"
+		response = requests.post(url, headers=self.get_headers(), data=json.dumps(payload))
+		return response
+
+	# upload job data
+	def job_upload(self, job_id, payload, content_type):
+		url = self.instance_url + self.API_ENDPOINT +"jobs/ingest/"+ job_id +"/batches/"
+		headers = self.get_headers()
+		headers["Content-Type"] = content_type
+		response = requests.put(url, headers=headers, data=payload.encode('utf-8'))
+		return response
+
+	# update job
+	def job_update(self, job_id, payload):
+		url = self.instance_url + self.API_ENDPOINT +"jobs/ingest/"+ job_id
+		response = requests.patch(url, headers=self.get_headers(), data=json.dumps(payload))
+		return response
+
+	# check job
+	def job_check(self, job_id):
+		url = self.instance_url + self.API_ENDPOINT +"jobs/ingest/"+ job_id
+		response = requests.get(url, headers=self.get_headers())
+		return response
+
+	# get job failures
+	def job_get_failed(self, job_id):
+		url = self.instance_url + self.API_ENDPOINT +"jobs/ingest/"+ job_id +"/failedResults"
+		response = requests.get(url, headers=self.get_headers())
+		return response
