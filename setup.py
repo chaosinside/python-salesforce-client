b518b0d356c6ae4957cc321b3de26b5838b8c54c
Chris Hubbard <chubbard@gmail.com>
2020-04-22 17:23:37 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SalesforceClient",
+    version="1.0.0",
+    author="Chris Hubbard",
+    author_email="chubbard@gmail.com",
+    description="A python client for Salesforce",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chaosinside/python-salesforce-client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
